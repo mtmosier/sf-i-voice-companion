b98d3924754c21b43dbb7334ffe268b46069d27e
Michael T. Mosier <mtmosier@gmail.com>
2020-04-27 17:38:37 -0500

    Update outputDebugInfo.cs
    Write debug output to a log file in addition to the VA console.

--- VoiceAttack/InternalFunctions/outputDebugInfo.cs
+++ VoiceAttack/InternalFunctions/outputDebugInfo.cs
@@ -1,10 +1,17 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Globalization;
 
 public class VAInline
 {
 	public void main()
 	{
+		//*** Initialize
+		DateTime localDate = DateTime.Now;
+		String debugOutput = "";
+		String message;
+
 		//*** Ship variables
 		string shipNameListStr = VA.GetText(">>shipNameListStr");
 		string[] shipNameList = shipNameListStr.Split(';');
@@ -25,12 +32,33 @@ public class VAInline
 		variable = VA.GetText(">>companionNameList");
 		string[] companionNameList = variable.Split(';');
 
+		message = localDate.ToString("u");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Pink");
 
-		VA.WriteToLog(typeof(string).Assembly.ImageRuntimeVersion, "Pink");
+		message = typeof(string).Assembly.ImageRuntimeVersion;
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Pink");
+
+		message = VA.ParseTokens("CPU Usage: {STATE_CPU}%");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Pink");
+
+		message = VA.ParseTokens("RAM Usage: {STATE_RAMAVAILABLE} / {STATE_RAMTOTAL}");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Pink");
+
+		message = VA.ParseTokens("Recording Device: {STATE_DEFAULTRECORDING} || {STATE_DEFAULTRECORDINGCOMMS}");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Pink");
 
 		string tmpVarName;
 		for (short s = 0; s < shipNameList.Length; s++) {
-			VA.WriteToLog(shipNameList[s] + " > isInUse: " + VA.GetBoolean(">>shipInfo[" + shipNameList[s] + "].isInUse"), "Orange");
+
+			message = shipNameList[s] + " > isInUse: " + VA.GetBoolean(">>shipInfo[" + shipNameList[s] + "].isInUse");
+			debugOutput += message + "\n";
+			VA.WriteToLog(message, "Orange");
+
 			if (VA.GetBoolean(">>shipInfo[" + shipNameList[s] + "].isInUse") == true) {
 				for (short w = 0; w < wgNameList.Length; w++) {
 					for (short n = 1; n <= maxWGNum; n++) {
@@ -39,13 +67,13 @@ public class VAInline
 							int? lenN = VA.GetInt(tmpVarName + ".weaponKeyPress.len");
 							int len = lenN.HasValue ? lenN.Value : 0;
 
-							// VA.WriteToLog(shipNameList[s] + " >" + wgNameList[w] + " " + n + " > isActive:  " + VA.GetBoolean(tmpVarName + ".isActive"), "Orange");
-							// VA.WriteToLog(shipNameList[s] + " >" + wgNameList[w] + " " + n + " > weaponKeyPress.len:  " + VA.GetInt(tmpVarName + ".weaponKeyPress.len"), "Orange");
 							List<string> keyPressList = new List<string>();
 							for (short l = 0; l < len; l++) {
 								keyPressList.Add(VA.GetText(tmpVarName + ".weaponKeyPress[" + l + "]"));
 							}
-							VA.WriteToLog(shipNameList[s] + " >" + wgNameList[w] + " " + n + " > weaponKeyPressList:  " + String.Join(",", keyPressList), "Orange");
+							message = shipNameList[s] + " >" + wgNameList[w] + " " + n + " > weaponKeyPressList:  " + String.Join(",", keyPressList);
+							debugOutput += message + "\n";
+							VA.WriteToLog(message, "Orange");
 						}
 					}
 				}
@@ -58,28 +86,94 @@ public class VAInline
 						int? lenN = VA.GetInt(tmpVarName + ".weaponKeyPress.len");
 						int len = lenN.HasValue ? lenN.Value : 0;
 
-						// VA.WriteToLog(shipNameList[s] + " >" + groupName + " > isActive:  " + VA.GetBoolean(tmpVarName + ".isActive"), "Orange");
-						// VA.WriteToLog(shipNameList[s] + " >" + groupName + " > weaponKeyPress.len:  " + VA.GetInt(tmpVarName + ".weaponKeyPress.len"), "Orange");
 						List<string> keyPressList = new List<string>();
 						for (short l = 0; l < len; l++) {
 							keyPressList.Add(VA.GetText(tmpVarName + ".weaponKeyPress[" + l + "]"));
 						}
-						VA.WriteToLog(shipNameList[s] + " >" + groupName + " > weaponKeyPressList:  " + String.Join(",", keyPressList), "Orange");
+
+						message = shipNameList[s] + " >" + groupName + " > weaponKeyPressList:  " + String.Join(",", keyPressList);
+						debugOutput += message + "\n";
+						VA.WriteToLog(message, "Orange");
 					}
 				}
 			}
 		}
 
-		VA.WriteToLog("Active Ship Name: " + VA.GetText(">>activeShipName"), "Orange");
-		VA.WriteToLog("Ship Name List: " + String.Join(", ", shipNameList), "Red");
-		VA.WriteToLog("Weapon Group Name List: " + String.Join(", ", wgNameList), "Red");
-		VA.WriteToLog("Emergency Group List: " + String.Join(", ", staticGroupList), "Red");
-		VA.WriteToLog("Companion Name List: " + String.Join(", ", companionNameList), "Red");
-		VA.WriteToLog("Selected Weapon Slot: " + VA.GetInt(">>selectedWeaponSlot"), "Green");
-		VA.WriteToLog("Cease Fire Active: " + VA.GetBoolean(">>ceaseFireActive"), "Green");
-		VA.WriteToLog("Game Voice Enabled: " + VA.GetBoolean(">>gameVoiceEnabled"), "Green");
-		VA.WriteToLog("Active Window Title: " + VA.ParseTokens("{ACTIVEWINDOWTITLE}"), "Blue");
-		VA.WriteToLog("Voice Directory: " + VA.GetText(">>voiceDir"), "Blue");
-		VA.WriteToLog("Companion: " + VA.GetText(">>companionName"), "Blue");
+		message = "Active Ship Name: " + VA.GetText(">>activeShipName");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Orange");
+
+		message = "Ship Name List: " + String.Join(", ", shipNameList);
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Red");
+
+		message = "Weapon Group Name List: " + String.Join(", ", wgNameList);
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Red");
+
+		message = "Emergency Group List: " + String.Join(", ", staticGroupList);
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Red");
+
+		message = "Companion Name List: " + String.Join(", ", companionNameList);
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Red");
+
+		message = "Selected Weapon Slot: " + VA.GetInt(">>selectedWeaponSlot");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Green");
+
+		message = "Cease Fire Active: " + VA.GetBoolean(">>ceaseFireActive");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Green");
+
+		message = "Game Voice Enabled: " + VA.GetBoolean(">>gameVoiceEnabled");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Green");
+
+		message = "Active Window Title: " + VA.ParseTokens("{ACTIVEWINDOWTITLE}");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Blue");
+
+		message = "Voice Directory: " + VA.GetText(">>voiceDir");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Blue");
+
+		message = "Companion: " + VA.GetText(">>companionName");
+		debugOutput += message + "\n";
+		VA.WriteToLog(message, "Blue");
+
+
+		debugOutput += "\n\n";
+		appendToDebugFile(debugOutput);
+	}
+
+
+	private bool appendToDebugFile(string fileContents) {
+		bool rtnVal = false;
+		string filePath = getSavePath();
+
+		if (!string.IsNullOrEmpty(filePath)) {
+			try {
+				System.IO.File.AppendAllText(filePath, fileContents);
+				rtnVal = true;
+			} catch (Exception e) {}
+		}
+
+		return rtnVal;
+	}
+
+	private string getSavePath() {
+		string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+		path = Path.Combine(path, "VoiceAttack SF-I Companion");
+
+		try {
+			Directory.CreateDirectory(path);
+			path = Path.Combine(path, "debug.log");
+		} catch (Exception e) {
+			path = null;
+		}
+
+		return path;
 	}
 }
